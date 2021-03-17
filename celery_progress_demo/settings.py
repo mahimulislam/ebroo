3bd647bf1501ec4cb1fd251953924c1ae27a6c2c
Mahimul Islam <mahimulislam@gmail.com>
2021-03-17 17:40:34 -0400

    “InitCommits”

--- celery_progress_demo/settings.py
+++ celery_progress_demo/settings.py
@@ -125,7 +125,9 @@ WSGI_APPLICATION = 'celery_progress_demo.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "/media/"
 STATIC_URL = '/static/'
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#BROKER_URL = 'redis://localhost:6379'
+BROKER_URL = 'redis://:pa6c40631fe3047169a013e1ff97c7cb34fba24d58c52b36d319ad1cdf7b354ce@ec2-54-166-2-65.compute-1.amazonaws.com:27989'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = '    redis://:pa6c40631fe3047169a013e1ff97c7cb34fba24d58c52b36d319ad1cdf7b354ce@ec2-54-166-2-65.compute-1.amazonaws.com:27989'
 CELERY_TASK_SERIALIZER=['json']
 django_heroku.settings(locals())
